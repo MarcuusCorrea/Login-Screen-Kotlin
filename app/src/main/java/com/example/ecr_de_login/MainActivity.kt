6bd9cdd464fd0d9269cf5d6f1f4a2e7adcc0535e
MarcuusCorrea <correa.marcospaulo@gmail.com>
2024-07-16 11:38:37 -0400

    first commit

---
+++ app/src/main/java/com/example/ecr_de_login/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.ecr_de_login
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecr_de_login.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEntrar.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editPassword.text.toString().trim()
+
+            if (username.equals("user") && password.equals("pass")){
+                Toast.makeText(applicationContext, "Login OK", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(applicationContext, "Login Inválido", Toast.LENGTH_SHORT).show()
+
+            }
+
+            binding.editUsername.setText("")
+            binding.editPassword.setText("")
+
+
+        }
+    }
+}
